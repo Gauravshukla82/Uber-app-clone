c3e86498abf82d2231b9608a3a945714721adafa
Gauravshukla82 <gauravshukla.ec1022@gmail.com>
2023-06-15 00:39:51 +0530

    map implemented

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,18 @@ const { driverPageRouter } = require("./routes/driverPage.routes");
 app.use("/drivers", driverRouter);
 app.use("/driverpage", driverPageRouter);
 require("dotenv").config();
-const {userRouter}=require("./router/user.router")
+const { userRouter } = require("./router/user.router");
 const port = process.env.port;
 app.use(express.json());
-app.use("/users",userRouter)
-app.listen(port,async(req,res)=>{
-    try {
-        await connection;
-      await connection1;
-        console.log("db is connected")
-        console.log(`Surver is running at ${port}`)
-    } catch (error) {
-        console.log(error)
-        console.log('there is something wrong with this')
-    }
+app.use("/users", userRouter);
+app.listen(port, async (req, res) => {
+  try {
+    await connection;
+    await connection1;
+    console.log("db is connected");
+    console.log(`Surver is running at ${port}`);
+  } catch (error) {
+    console.log(error);
+    console.log("there is something wrong with this");
+  }
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <Provider store={store}>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </BrowserRouter>
