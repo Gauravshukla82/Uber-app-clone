3ec136e793e385ee006bea40ed9aee33b62a7763
Ruchi <ruchiagrawal9186@gmail.com>
2023-06-16 23:32:09 -0800

    payment page done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,13 @@ import { BrowserRouter } from "react-router-dom";
  import { ChakraProvider } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //  <Provider store={store}>
+ 
     <BrowserRouter>
       <ChakraProvider >
              <App />
       </ChakraProvider> 
     </BrowserRouter>
-  // </Provider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
